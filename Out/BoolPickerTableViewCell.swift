45e5d27044481858d46eb877402c77090a1db99e
Eddie Chen <agameofprivacy@gmail.com>
2014-10-19 20:18:41 -0400

    Canvas TableView Cells Classes created

---
+++ Out/BoolPickerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BoolPickerTableViewCell.swift
+//  Out
+//
+//  Created by Eddie Chen on 10/19/14.
+//  Copyright (c) 2014 Coming Out App. All rights reserved.
+//
+
+import UIKit
+
+class BoolPickerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Out/EventInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventInfoTableViewCell.swift
+//  Out
+//
+//  Created by Eddie Chen on 10/19/14.
+//  Copyright (c) 2014 Coming Out App. All rights reserved.
+//
+
+import UIKit
+
+class EventInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
