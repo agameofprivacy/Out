e301b1026ea2022662de7b53f4a886878433f67c
Eddie Chen <agameofprivacy@gmail.com>
2014-11-01 22:42:47 -0400

    additional peopleTableView setup

--- Out/PeopleTabViewController.swift
+++ Out/PeopleTabViewController.swift
@@ -13,6 +13,7 @@ class PeopleTabViewController: UIViewController, UITableViewDelegate, UITableVie
     var peopleTableView:TPKeyboardAvoidingTableView!
     var mentorCellOverlay:UIView!
     var segmentedControlView:UIView!
+    var segmentedControl:UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,10 +21,15 @@ class PeopleTabViewController: UIViewController, UITableViewDelegate, UITableVie
 
         self.peopleTableView = TPKeyboardAvoidingTableView(frame: self.view.frame)
         self.peopleTableView.setTranslatesAutoresizingMaskIntoConstraints(false)
-        self.peopleTableView.contentInset = UIEdgeInsets(top: 144.0, left: 0, bottom:50, right: 0)
+        self.peopleTableView.contentInset = UIEdgeInsets(top: 144.0, left: 0, bottom:0, right: 0)
         self.peopleTableView.registerClass(PersonTableViewCell.self, forCellReuseIdentifier: "PersonTableViewCell")
         self.peopleTableView.backgroundColor = UIColor.whiteColor()
         self.peopleTableView.frame = self.view.frame
+        self.peopleTableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        self.peopleTableView.rowHeight = UITableViewAutomaticDimension
+        self.peopleTableView.estimatedRowHeight = 90
+        self.peopleTableView.delegate = self
+        self.peopleTableView.dataSource = self
         self.view.addSubview(self.peopleTableView)
         println(self.peopleTableView.frame)
         
@@ -33,10 +39,16 @@ class PeopleTabViewController: UIViewController, UITableViewDelegate, UITableVie
         
         self.segmentedControlView = UIView(frame: CGRectZero)
         self.segmentedControlView.setTranslatesAutoresizingMaskIntoConstraints(false)
-        self.segmentedControlView.backgroundColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1)
+        self.segmentedControlView.backgroundColor = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1)
+        
+        self.segmentedControl = UISegmentedControl(items: ["Following","Followers"])
+        self.segmentedControl.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.segmentedControl.tintColor = UIColor.blackColor()
+        self.segmentedControl.selectedSegmentIndex = 0
         
         self.view.addSubview(self.mentorCellOverlay)
         self.view.addSubview(self.segmentedControlView)
+        self.segmentedControlView.addSubview(self.segmentedControl)
         
         var viewsDictionary = ["mentorCellOverlay":mentorCellOverlay, "segmentedControlView":segmentedControlView]
         var metricsDiciontary = ["margin":0]
@@ -46,15 +58,29 @@ class PeopleTabViewController: UIViewController, UITableViewDelegate, UITableVie
         self.view.addConstraints(verticalConstraints)
         self.view.addConstraints(horizontalConstraints)
         
+        var segmentsViewsDictionary = ["segmentedControl":segmentedControl]
+        var segmentsMetricsDictionary = ["margin":12]
+        
+        var segmentsHorizontalConstraints:Array = NSLayoutConstraint.constraintsWithVisualFormat("H:|-margin-[segmentedControl]-margin-|", options: NSLayoutFormatOptions(0), metrics: segmentsMetricsDictionary, views: segmentsViewsDictionary)
+        var segmentsVerticalConstraints:Array = NSLayoutConstraint.constraintsWithVisualFormat("V:|-8-[segmentedControl]-8-|", options: NSLayoutFormatOptions.AlignAllCenterX, metrics: segmentsMetricsDictionary, views: segmentsViewsDictionary)
+        
+        self.segmentedControlView.addConstraints(segmentsHorizontalConstraints)
+        self.segmentedControlView.addConstraints(segmentsVerticalConstraints)
         
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell:PersonTableViewCell = tableView.dequeueReusableCellWithIdentifier("PersonTableViewCell") as PersonTableViewCell
+        if indexPath.row % 2 == 0 {
+            cell.backgroundColor = UIColor.whiteColor()
+        }
+        else{
+            cell.backgroundColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1)
+        }
         return cell
     }
 
